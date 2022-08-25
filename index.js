0a2349c6f1f2f9c8f7ef8f70e328c16bfa10a98f
Jack Smallwood <jacksmallwood@icloud.com>
2022-08-25 18:05:02 +0100

    installed npm inquirer

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
+const {prompt} = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  { type: "input", name: "hello", message: "What is your name?" },
+  {
+    type: "input",
+    name: "project",
+    message: "what is the name of your project?",
+  },
+  {
+    type: "checkbox",
+    name: "type",
+    message: "which tech did you use?",
+    choices: ["Javascript", "HTML", "CSS"],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  prompt(questions).then((myName) => {
+    generateMarkdown(myName);
+  });
+}
 
 // Function call to initialize app
 init();
